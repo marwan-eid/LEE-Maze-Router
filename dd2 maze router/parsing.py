e73a5ed29f7a444dd7e03179514bde7e7cc0c247
doniaGhazy <45397911+doniaGhazy@users.noreply.github.com>
2020-07-15 03:05:38 +0200

    Parsing the PINs part with their associated NETs

---
+++ dd2 maze router/parsing.py
@@ -0,0 +1,25 @@
+import re
+mybuffer = []
+with open("file.txt", "r") as paragraphs_file: 
+    i = 0
+    for paragraph in paragraphs_file:
+        if paragraph.startswith('PINS'):
+            #print(paragraph)
+            for word in paragraph.split():
+                try:
+                    a = int(word)
+                    #print(a)
+                except ValueError:
+                    pass
+        if "+ LAYER" in paragraph:
+           # print(paragraph)
+            res = paragraph.split(' ')[4]
+            s = re.search(r"\d+(\.\d+)?", res)
+        if "PLACED" in paragraph and paragraph.startswith(' '):
+            #print(paragraph)
+            test_list = [int(d) for d in re.findall(r'-?\d+', paragraph)]
+            result = str(test_list)[1:-1]
+            print('net' + str(i), '(', s.group(0), ',', result, ')' )
+            i+=1
+
+            #print(result)
