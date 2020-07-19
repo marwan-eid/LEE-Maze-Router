e5b7f4952a09838f53cecc182d6692ddc97aaae5
doniaGhazy <45397911+doniaGhazy@users.noreply.github.com>
2020-07-19 03:38:46 +0200

    Update parsing.py
    Looping over all the macros inside the lef file and getting all the macros with their corresponding pins

--- dd2 maze router/parsing.py
+++ dd2 maze router/parsing.py
@@ -2,7 +2,7 @@ import re
 import itertools
 import os
 Text = open("file.txt", "r")
-file1 = open("write.txt", "w")  
+file1 = open("write.txt", "w")
 x = Text.read()
 pattern = r'^(NETS.*END NETS)'
 second = re.search(pattern, x, re.MULTILINE | re.DOTALL)
@@ -17,18 +17,18 @@ with open("write.txt", "r") as paragraphs_file:
                 except ValueError:
                     pass
         if paragraph.startswith('NETS') or paragraph.startswith('END'):
-            continue 
+            continue
         else:
             string = paragraph.split()
         if string[0].startswith('('):
             del string[0]
-        if len(string) >2:
+        if len(string) > 2:
             del string[2]
-        if len(string) >2:
+        if len(string) > 2:
             del string[2]
         #string basically is a list that contains all the parsed nets with its corrosponding pins from def
         #print(string)
-    
+
 
 elf_Text = open("lef.txt", "r")
 elf_file = open("file_elf.txt", "w")
@@ -39,14 +39,15 @@ elf_file.write(elf_second.group(0))
 elf_file2 = open("file_elf.txt", "r")
 elf_x2 = elf_file2.read()
 
-i=0
-macro_second=[]
+i = 0
+macro_second = []
 macro_second2 = []
-pins_name=[]
-layer_name=[]
-pin_positions=[]
-b=[]
-arg=[]
+macro_second3=[]
+pins_name = []
+layer_name = []
+pin_positions = []
+b = []
+arg = []
 dicts = {}
 with open("file_elf.txt", "r") as lef_paragraphs_file:
     for lef_paragraph in lef_paragraphs_file:
@@ -55,54 +56,48 @@ with open("file_elf.txt", "r") as lef_paragraphs_file:
            lef_macro_final = 'END' + " " + lef_macro
            lef_macro_start = 'MACRO' + " " + lef_macro
            #print(lef_macro)
-           macro_second.append(lef_macro)
+           macro_second.append(lef_macro_final)
            macro_second2.append(lef_macro_start)
-           i+=1
-           #dicts.update({i: lef_macro})
-        else: 
+           macro_second3.append(lef_macro)
+           i += 1
+        else:
             continue
-        
-z=0
-#for x in range (len(macro_second)):
-elf_file_macro = open("donia.txt", "r+")
-content = elf_file_macro.read()
-pin_count = sum(
-    1 for match in re.finditer(r"\bPIN\b", content))
-#macro_second.pop(0)
-elf_pattern2 = r'^' + re.escape(macro_second2[0]) + \
-    '.+' + re.escape(macro_second[0])
-elf_second2 = re.search(elf_pattern2, elf_x2,
-                    re.MULTILINE | re.DOTALL)
-elf_file_macro.seek(0)
-elf_file_macro.truncate()
-elf_file_macro.write((elf_second2.group(0)))
-elf_file_macro.close()
-with open("donia.txt", "r+") as lef_macros_file:
-    for lef_macros in lef_macros_file:
-        if lef_macros.startswith(" ") or lef_macros.startswith("   "):
-            temp_pin = lef_macros.split()
-            #print(temp_pin)
-            if 'PIN' in temp_pin:
-                lef_pin = lef_macros.split(' ')[3]
-                pins_name.append(lef_pin)
-            if 'LAYER' in temp_pin:
-                lef_layer = lef_macros.split(' ')[7]
-                #print(lef_layer)
-                layer_name.append(lef_layer)
-            if 'RECT' in temp_pin:
-                pin_positions.append(temp_pin)
-                #print(lef_layer)
 
-
-    #conc = {lef_macro: pins_name[m], left_macro: pin_name[m+1]}
-    #dicts.setdefault(macro_second, {})[pins_name] = 1
-    dicts.setdefault(macro_second[0], []).append(pins_name)
-    #dicts.update({lef_macro: pins_name[1]})
-    '''IDs = dicts['FILL\n']
-    Defaults = macro_second
-    D = dict.fromkeys(IDs, Defaults)'''
-    print(dicts)
-
+z = 0
+for x in range (len(macro_second2)):
+    elf_file_macro = open("donia.txt", "r+")
+    content = elf_file_macro.read()
+    pin_count = sum(
+        1 for match in re.finditer(r"\bPIN\b", content))
+    #macro_second.pop(0)
+    z+=1
+    #print(z,pin_count)
+    elf_pattern2 = r'^' + re.escape(macro_second2[x]) + \
+        '.+' + re.escape(macro_second[x])
+    elf_second2 = re.search(elf_pattern2, elf_x2,
+                            re.MULTILINE | re.DOTALL)
+    elf_file_macro.seek(0)
+    elf_file_macro.truncate()
+    elf_file_macro.write((elf_second2.group(0)))
+    elf_file_macro.close()
+    with open("donia.txt", "r") as lef_macros_file:
+        for lef_macros in lef_macros_file:
+            if lef_macros.startswith(" ") or lef_macros.startswith("   "):
+                temp_pin = lef_macros.split()
+                #print(temp_pin)
+                if 'PIN' in temp_pin:
+                    lef_pin = lef_macros.split(' ')[3]
+                    pins_name.append(lef_pin)
+                if 'LAYER' in temp_pin:
+                    lef_layer = lef_macros.split(' ')[7]
+                    #print(lef_layer)
+                    layer_name.append(lef_layer)
+                if 'RECT' in temp_pin:
+                    pin_positions.append(temp_pin)
+        for f in range(pin_count):
+            dicts.setdefault(macro_second3[x], []).append(pins_name[0])
+            popp = pins_name.pop(0)
+            #print(popp)
 
 def centroid(vertexes):
     _x_list = [vertex[0] for vertex in vertexes]
@@ -111,24 +106,32 @@ def centroid(vertexes):
     _x = sum(_x_list) / _len
     _y = sum(_y_list) / _len
     return(_x, _y)
-c=[]
-final2=[]
-new_position=[]
-pin_position=[]
+
+
+c = []
+final2 = []
+new_position = []
+pin_position = []
+
+
 def ayhaga(list):
     for i in list:
         b.append(float(i))
     return b
+
+
 def convert(list):
     return tuple(list)
-R=[]
-new_temp=[]
+
+
+R = []
+new_temp = []
 for m in range(pin_count):
     pin_positions[m].pop(-1)
     pin_positions[m].pop(0)
     pin_positions[m] = [float(i) for i in pin_positions[m]]
     for d in range(4):
-        temp = pin_positions[m] 
+        temp = pin_positions[m]
         new_temp.append(temp[d]*100)
     final2 = centroid(
         convert(zip(new_temp[::2], new_temp[1::2])))  # (0.5, 0.5)
@@ -142,13 +145,11 @@ for m in range(pin_count):
 combined = dict(zip(pins_name, pin_position))
 combined2 = dict(zip(pins_name, layer_name))
 
-
 def common_entries(*dcts):
     for i in set(dcts[0]).intersection(*dcts[1:]):
         yield (i,) + tuple(d[i] for d in dcts)
 
-print(list(common_entries(combined, combined2)))
-'''
-print(combined)
-print(combined2)
-'''
+
+#print(list(common_entries(combined, combined2)))
+
+print(dicts)
